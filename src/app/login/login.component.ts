46463cd5169189106781cbcadaf024164f0eea45
haalqershi <49689391+haalqershi@users.noreply.github.com>
2022-08-04 15:21:26 -0400

    Adding login button and login function

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService){}
+
+  login(){
+    this.authService.login();
+  }
 
   ngOnInit(): void {
   }
