c1ce93aae4b6693262ee431999def9f901e83f4c
haalqershi <49689391+haalqershi@users.noreply.github.com>
2022-08-07 15:28:01 -0400

    implemented products filtering based on category

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,7 +1,9 @@
+import { ActivatedRoute } from '@angular/router';
 import { CategoryService } from './../category.service';
 import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Product } from '../Product';
 
 @Component({
   selector: 'app-products',
@@ -10,14 +12,29 @@ import { Observable } from 'rxjs';
 })
 export class ProductsComponent implements OnInit {
 
-  products$: Observable<any[]>;
+  products:  any[] = [];
   categories$: Observable<any[]>;
+  category: any;
+  filteredProducts: any[] = [];
 
   constructor(
     private productService: ProductService,
-    private categoryService: CategoryService) {
-    this.products$ = this.productService.getAll();
+    private categoryService: CategoryService,
+    private route: ActivatedRoute) { // todo: decouple the nested subscribtion 
+    this.productService.getAll().subscribe( products =>{
+      this.products = products;
+      
+      route.queryParamMap.subscribe(params =>{
+        this.category = params.get('category');
+  
+        this.filteredProducts = (this.category) ? 
+          this.products?.filter((p: any) => p.val.category === this.category) : this.products;
+      });
+    });
+
     this.categories$ = this.categoryService.getAll();
+
+   
   }
 
   ngOnInit(): void {
