bb2d479fc6f60590357912b8d99f4451630c8fc3
haalqershi <49689391+haalqershi@users.noreply.github.com>
2022-08-13 05:54:15 -0400

    finished authentication

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 import { AuthService } from '../auth.service';
 import { Order } from '../models/order';
 import { OrderService } from '../order.service';
+import { UserModel } from '../models/user-model';
 
 @Component({
   selector: 'app-shipping',
@@ -15,21 +16,27 @@ export class ShippingComponent implements OnInit, OnDestroy {
 
   shipping : any = {}; 
   userSubscription!: Subscription;
-  currentUserId!: string;
+  currentUserId!: any;
   @Input('cart') cart!: ShoppingCart;
 
   constructor(private orderService: OrderService, private authService: AuthService, private router: Router){
   }
 
   ngOnInit(): void {
-    this.userSubscription = this.authService.user$.subscribe((user:any) => this.currentUserId = user.uid);
+    this.userSubscription = this.authService.user$.subscribe((user:any) => this.currentUserId = user.email);
   }
 
-  async placeOrder() {
-    let order = new Order(this.currentUserId, this.shipping, this.cart);
-    let result = await this.orderService.placeOrder(order.getOrder());
+  placeOrder() {
+    this.authService.user$.subscribe((user:any) => {
+      console.log(user);
+      this.currentUserId = user.email;
+      let order = new Order(user.email, this.shipping, this.cart);
+      let result = this.orderService.placeOrder(order.getOrder());
+      // this.router.navigate(['/order/confirmation', result.key]);
+    });
+   
     // this.shoppingCartService.clearShoppingCart();
-    this.router.navigate(['/order/confirmation', result.key]);
+    // this.router.navigate(['/order/confirmation', result.key]);
   } 
 
   ngOnDestroy(): void {
